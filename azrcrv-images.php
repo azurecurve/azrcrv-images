3b5e09b6b721c65273fd6adba95afb37c2b52bc8
azurecurve <github@azurecurve.co.uk>
2025-05-31 11:19:08 +0100

    Release v1.2.6

--- azrcrv-images.php
+++ azrcrv-images.php
@@ -3,8 +3,9 @@
  * ------------------------------------------------------------------------------
  * Plugin Name:		Images
  * Description:		Shortcode to display image in post; add width, alt, caption or error attributes.
- * Version:			1.2.5
+ * Version:			1.2.6
  * Requires CP:		1.0
+ * Requires PHP:	7.4
  * Author:			azurecurve
  * Author URI:		https://development.azurecurve.co.uk/classicpress-plugins/
  * Plugin URI:		https://development.azurecurve.co.uk/classicpress-plugins/images/
